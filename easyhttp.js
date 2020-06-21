a994614d9b2e6efafe67256b9023be8303d984c8
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2020-06-21 23:01:01 +0200

    Big Update (Basic UI, async/await)

--- easyhttp.js
+++ easyhttp.js
@@ -2,21 +2,28 @@
  * EasyHTTP Library
  * Library for making HTTP requests
  *
- * @version 2.0.0
+ * @version 3.0.0
  * @author  Brad Traversy
  * @license MIT
  *
  **/
 
 class EasyHTTP {
+  // Make an HTTP GET Request
+  async get(url) {
+    //make a response Object:
+    const response = await fetch(url);
 
-    // Make an HTTP GET Request
-    get(url) {
-        return new Promise((resolve, reject) => {
-            fetch(url)
-                .then(res => res.json())
-                .then(data => resolve(data))
-                .catch(err => reject(err));
-        });
-    }
+    //convert the response from JSON into JS Object and return it:
+    const resData = await response.json();
+    return resData;
+
+    // VERSION WITHOUT ASYNC/AWAIT:
+    // return new Promise((resolve, reject) => {
+    //   fetch(url)
+    //     .then((res) => res.json())
+    //     .then((data) => resolve(data))
+    //     .catch((err) => reject(err));
+    // });
+  }
 }
--- heroclash.js
+++ heroclash.js
@@ -1,11 +1,26 @@
-const http = new EasyHTTP;
+const http = new EasyHTTP();
 
 //Get one random character:
-function randomCharacter() {
-    const id = Math.floor(Math.random() * 731 + 1);
-    http.get(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/3521494294562439/${id}`)
-        .then(data => console.log(data))
-        .catch(err => console.log(err));
+async function randomCharacter() {
+  const id = Math.floor(Math.random() * 731 + 1);
+  const response = await http.get(
+    `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/3521494294562439/${id}`
+    // "heroes.json"
+    // "herostats.json"
+  );
+  console.log(response);
+  return response;
 }
 
-document.getElementById("randomHero").addEventListener("click", randomCharacter);
+document
+  .getElementById("randomHero")
+  .addEventListener("click", randomCharacter);
+
+class Game {
+  player1 = new Player();
+  player2 = new Player();
+}
+
+class Player {
+  Player() {}
+}
