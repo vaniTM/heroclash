3fb2da9cd55a4fd18abd59366dce34b3a0e998f2
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2020-06-25 23:20:37 +0200

    turn cards on click, update card with button-click

--- heroclash.js
+++ heroclash.js
@@ -4,8 +4,9 @@ class Heroclash {
   images1 = [];
   images2 = [];
 
-  async init(numberOfCards) {
-    // fetch herostats:
+  //load data from json-file and save to localStorage
+  async loadData() {
+    //fetch hero data:
     let response = await fetch("herostats.json");
     const herodata = await response.json();
 
@@ -13,45 +14,77 @@ class Heroclash {
     response = await fetch("heroimages.json");
     const images = await response.json();
 
+    //save to localStorage
+    localStorage.setItem("allHeroes", JSON.stringify(herodata));
+    localStorage.setItem("allImages", JSON.stringify(images));
+    location.reload();
+  }
+
+  //start the game with the chosen deckSize
+  start(deckSize) {
+    let herodata;
+    let images;
+
+    //load herodata from localStorage
+    herodata = JSON.parse(localStorage.getItem("allHeroes"));
+    images = JSON.parse(localStorage.getItem("allImages"));
+
     let ids = [];
 
-    for (let i = 0; i < numberOfCards; i++) {
-      let id = Math.floor(Math.random() * 731 + 1);
+    //refactoring needed! nasty code duplication
+    while (this.stats1.length < deckSize) {
+      let id = Math.floor(Math.random() * 731);
       while (!ids.includes(id)) {
         if (this.validStats(herodata[id])) {
           this.stats1.push(herodata[id]);
           this.images1.push(images[id]);
-          ids.push(id);
+        } else {
+          id = (id + 1) % 731;
         }
+        ids.push(id);
       }
-      id = Math.floor(Math.random() * 731 + 1);
+    }
+    while (this.stats2.length < deckSize) {
+      let id = Math.floor(Math.random() * 731);
       while (!ids.includes(id)) {
         if (this.validStats(herodata[id])) {
           this.stats2.push(herodata[id]);
           this.images2.push(images[id]);
-          ids.push(id);
+        } else {
+          id = (id + 1) % 731;
         }
+        ids.push(id);
       }
     }
   }
 
-  //TODO: not working! :(
+  //checks if hero-stats contain a null-value
   validStats(hero) {
-    for (let key in hero) {
-      if (hero.key === null) {
-        return false;
-      }
+    let validStats = true;
+    if (
+      hero.intelligence === "null" ||
+      hero.durability === "null" ||
+      hero.speed === "null" ||
+      hero.strength === "null" ||
+      hero.combat === "null" ||
+      hero.power === "null"
+    ) {
+      validStats = false;
     }
-    return true;
+    return validStats;
   }
 }
 
-const game = new Heroclash();
+//-------------------------------------------------------------------
+class Player {}
 
-game.init(3);
-console.log(game.stats1);
-console.log(game.images1);
-console.log(game.stats2);
-console.log(game.images2);
+//-------------------------------------------------------------------
 
-game.validStats(game.stats1[0]);
+// const game = new Heroclash();
+// // if (localStorage.getItem("allHeroes") === null) {
+// //   game.loadData();
+// // }
+// // game.start(3);
+
+// console.log(game.stats1);
+// console.log(game.stats2);
---
+++ ui.js
@@ -0,0 +1,95 @@
+class UI {
+  //cards:
+  cards = document.querySelectorAll(".card");
+
+  //images:
+  image1 = document.querySelector("#image1");
+  image2 = document.querySelector("#image2");
+
+  //stats:
+  intelligence1 = document.querySelector("#intelligence1");
+  strength1 = document.querySelector("#strength1");
+  speed1 = document.querySelector("#speed1");
+  durability1 = document.querySelector("#durability1");
+  power1 = document.querySelector("#power1");
+  combat1 = document.querySelector("#combat1");
+
+  intelligence2 = document.querySelector("#intelligence2");
+  strength2 = document.querySelector("#strength2");
+  speed2 = document.querySelector("#speed2");
+  durability2 = document.querySelector("#durability2");
+  power2 = document.querySelector("#power2");
+  combat2 = document.querySelector("#combat2");
+
+  //set event handlers
+
+  constructor() {
+    const game = new Heroclash();
+
+    if (localStorage.getItem("allHeroes") === null) {
+      game.loadData();
+    }
+    game.start(3);
+
+    console.log(game.stats1);
+    console.log(game.stats2);
+  }
+
+  updateCards = function () {
+    document.getElementById("card-inner1").innerHTML = `
+    <div class="card-front">
+              <img
+                src="images/card-red.png"
+                width="330px"
+                alt="A red Playing Card"
+              />
+            </div>
+            <div class="card-back">
+              <div class="card-image">
+                <img
+                  id="image1"
+                  src="https://www.superherodb.com/pictures2/portraits/10/100/727.jpg"
+                  alt="Avatar"
+                />
+              </div>
+              <div id="stats1" class="stats">
+                <h2>Someone Else</h2>
+  
+                <ul>
+                  <li id="intelligence1">
+                    <i class="fas fa-brain"> </i>Intelligence<span
+                      style="margin-left: auto;"
+                      >100</span
+                    >
+                  </li>
+                  <li id="strength1">
+                    <i class="fas fa-dumbbell"></i>Strength
+                  </li>
+                  <li id="speed1">
+                    <i class="fas fa-tachometer-alt"></i>Speed
+                  </li>
+                  <li id="durability1">
+                    <i class="fas fa-shield-alt"></i>Durability
+                  </li>
+                  <li id="power1"><i class="fas fa-fist-raised"></i>Power</li>
+                  <li id="combat1"><i class="fas fa-khanda"></i>Combat</li>
+                </ul>
+              </div>
+    `;
+  };
+
+  // turnCard(card) {
+  //   card.style.transform = "rotateY(180deg)";
+  // }
+}
+
+ui = new UI();
+//rotate cards on click
+document.getElementById("card-inner1").addEventListener("click", function () {
+  this.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card-inner2").addEventListener("click", function () {
+  this.style.transform = "rotateY(180deg)";
+});
+
+document.getElementById("next").addEventListener("click", ui.updateCards);
